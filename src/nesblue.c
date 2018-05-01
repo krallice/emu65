3f2d4525f53dad533d5014ea3b249bbfc42d4b9e
krallice <sroz9960@gmail.com>
2018-05-01 17:32:19 +1000

    new nes_t struct

--- src/nesblue.c
+++ src/nesblue.c
@@ -5,67 +5,65 @@
 #define STEP_DURATION 10e6 // 10 ms
 #define ONE_SECOND 1e9
 
+typedef struct nes_t {
+	core_t *core;
+} nes_t;
+
+nes_t *init_nes() {
+
+	nes_t *nes = (nes_t*)malloc(sizeof(nes_t));
+	memset(nes, 0, sizeof(nes_t));
+
+	nes->core = init_core();
+
+	return nes;
+}
+
 // Sleep for STEP_DURATION nanoseconds:
-void nesblue_sleep(void) {
+void nes_sleep(void) {
         struct timespec req, rem;
         req.tv_sec = 0;
         req.tv_nsec = STEP_DURATION;
         nanosleep(&req, &rem);
 }
 
-// Neslite execution cycle:
-void run_nesblue(core_t *core) {
+void execute_nes(nes_t *nes) {
 
 	// Evenly spread cycles over the period of a second to achieve NESBLUE_CPU_FREQ:
         uint16_t cycles_per_step = (NESBLUE_CPU_FREQ / (ONE_SECOND / STEP_DURATION));
-	printf("Cycles Per Step %d\n", cycles_per_step);
 
         while (1) {
 
 		// Execute CPU Instructions:
-		for ( core->cyclecount %= cycles_per_step; core->cyclecount < cycles_per_step; ) {
-
-			if ( core->cyclecount != interruptnmi ) {
-				if ( core->cyclecount != 0 && (core->cyclecount % 750) == 0 ) {
-					printf("NMI TRIGGERED -- %d\n", core->cyclecount);
-					core->interruptstate = interruptnmi;
-				} else if ( core->cyclecount != 0 && (core->cyclecount % 1423) == 0 ) {
-					printf("TRIGGERED -- %d\n", core->cyclecount);
-					core->interruptstate = interruptirq;
-				}
-			}
-
-			step_core(core);
+		for ( nes->core->cyclecount %= cycles_per_step; nes->core->cyclecount < cycles_per_step; ) {
+			step_core(nes->core);
                 }
 
 		// Sleep the rest of our interval:
-                nesblue_sleep();
+                nes_sleep();
 	}
 }
 
 int main(void) {
 
-	core_t *core = init_core();
+	nes_t *nes = init_nes();
+	core_t *core = nes->core;
 
 	core->ram[0x0000] = LDX_I;
 	core->ram[0x0001] = 0x03;
-	//core->ram[0x0002] = 0x22;
 
 	core->ram[0x0003] = BRK;
 	core->ram[0x0004] = JMP_A;
 	core->ram[0x0005] = 0x00;
 	core->ram[0x0006] = 0x00;
 
-	//core->ram[0x3040] = 0x22; 
-	//core->ram[0x3041] = 0x22; 
 	core->ram[0x3042] = RTI; // Return From Interrupt
 
 	// .data
 	core->ram[0xFFFE] = 0x40;
 	core->ram[0xFFFF] = 0x30;
 
-	run_nesblue(core);
+	execute_nes(nes);
 
-	dump_core_state(core);
 	return 0;
 }
